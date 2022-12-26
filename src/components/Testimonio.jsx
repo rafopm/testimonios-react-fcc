77172f9a9fb5c664e041ef0ed8844cab20bd3923
rafopm <rafopm@gmail.com>
2022-12-25 23:10:51 -0500

    add data file

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import '../styles/Testimonio.css'
 
-function Testimonio(props){
+function Testimonio({
+  nombre,
+  pais,
+  imagen,
+  cargo,
+  empresa,
+  testimonio
+}) {
   return (
     <div className='contenedor-testimonio'>
       <img 
         className='imagen-testimonio'
-        src={require(`../imagenes/${props.imagen}.png`)}
-        alt='Foto de Emma' />
+        src={require(`../imagenes/${imagen}.png`)}
+        alt={`Foto de ${nombre}`} />
         <div className='contenedor-texto-testimonio'>
           <p className='nombre-testimonio'>
-            <strong>{props.nombre}</strong> en {props.pais}</p>
-          <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
-          <p className='texto-testimonio'>"{props.testimonio}"</p>
+            <strong>{nombre}</strong> en {pais}</p>
+          <p className='cargo-testimonio'>{cargo} en <strong>{empresa}</strong></p>
+          <p className='texto-testimonio'>"{testimonio}"</p>
         </div>
     </div>
   );
